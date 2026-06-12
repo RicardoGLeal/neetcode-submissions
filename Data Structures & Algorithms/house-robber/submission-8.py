6c169cbeb253adccf4c69dc7b675c63a69130c89
Ricardo Glez Leal <53849239+RicardoGLeal@users.noreply.github.com>
2026-06-12 16:54:32 -0700

    Add: house-robber - submission-8

---
+++ Data Structures & Algorithms/house-robber/submission-8.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+
+        prev1, prev2 = 0, 0
+
+        for idx, num in enumerate(nums):
+            if idx == 0:
+                prev1 = nums[0]
+            elif idx == 1:
+                prev1, prev2 = max(nums[1], prev1), prev1
+            else:
+                curr = max(prev1, prev2 + num)
+                prev2 = prev1
+                prev1 = curr
+
+        return prev1
