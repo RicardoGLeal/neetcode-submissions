5aef603084fe18f32280519c2a5baa20d90971e4
Ricardo Glez Leal <53849239+RicardoGLeal@users.noreply.github.com>
2026-06-12 16:30:26 -0700

    Add: house-robber - submission-4

---
+++ Data Structures & Algorithms/house-robber/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+            
+        maxAcum = [0] * len(nums)
+
+        for idx, num in enumerate(nums):
+            if idx == 0:
+                maxAcum[0] = num
+            elif idx == 1:
+                maxAcum[1] = max(nums[0], num)
+            else:
+                maxAcum[idx] = max(maxAcum[idx-1], maxAcum[idx-2] + num)
+        return maxAcum[-1]
+
+                
+        
