43ce2be79d54af4305b0f2c0247773a974428a01
Ricardo Gonzalez Leal <53849239+RicardoGLeal@users.noreply.github.com>
2026-08-11 13:27:42 -0600

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        prev1, prev2 = 1, 1
+
+        for i in range(n-1, 0, -1):
+            temp = prev1
+            prev1 = prev1 + prev2
+            prev2 = temp
+        return prev1
