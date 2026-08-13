1c5da0936b65e1b71f04d05f14f0f4e3506fd08a
Ricardo Gonzalez Leal <53849239+RicardoGLeal@users.noreply.github.com>
2026-08-13 13:36:30 -0600

    Add: count-paths - submission-7

---
+++ Data Structures & Algorithms/count-paths/submission-7.py
@@ -0,0 +1,17 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[-1] * n for _ in range(m)]
+        def dfs(x, y):
+            if x == m - 1 and y == n - 1:
+                return 1
+            
+            if x >= m or y >= n:
+                return 0
+
+            if dp[x][y] != -1:
+                return dp[x][y]
+            
+            dp[x][y] = dfs(x + 1, y) + dfs(x, y + 1)
+            return dp[x][y]
+        return dfs(0,0)
+
